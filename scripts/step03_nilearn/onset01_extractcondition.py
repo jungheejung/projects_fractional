89609821997edd8ebbd4757d8fe72afe17cd2428
jungheejung <heejung.jung@colorado.edu>
2023-06-26 12:40:55 -0400

    DEV: code for more generic extraction

--- scripts/step03_nilearn/onset01_extractcondition.py
+++ scripts/step03_nilearn/onset01_extractcondition.py
@@ -34,44 +34,49 @@ __status__ = "Development"
 # 'event03_question_onset', 'event04_response_onset',
 # 'event04_RT','accuracy'
 
-sub = 'sub-0009'
-ses = 'ses-04'
-main_dir = '/Users/h/Documents/projects_local/spacetop_fractional_analysis'
-beh_inputdir = join(main_dir, 'data/dartmouth/d02_preprocessed')
-beh_savedir = join(main_dir, 'data' , 'beh', 'beh03_bids', sub)
-Path(beh_savedir).mkdir( parents=True, exist_ok=True )
-beh_fpath = join(beh_inputdir, sub, 'sub-0009_ses-04_task-fractional_run-02-tomsaxe_beh.csv')
-beh_df = pd.read_csv(beh_fpath)
+# sub = 'sub-0009'
+# ses = 'ses-04'
+current_path = Path.cwd()
+main_dir = current_path.parent.parent #'/Users/h/Documents/projects_local/spacetop_fractional_analysis'
+beh_inputdir = join(main_dir, 'data', 'beh', 'beh02_preproc')
 
-subset_beh = beh_df[['event02_filetype', 'event02_story_onset','event03_question_onset', 'event04_response_onset','event04_RT','accuracy']]
+saxe_flist = glob.glob(join(beh_inputdir, '**', '*tomsaxe*.csv'), recursive=True)
+for saxe_fpath in sorted(saxe_flist):
+    saxe_fname = os.path.basename(saxe_fpath)
+    sub_bids = re.search(r'sub-\d+', saxe_fname).group(0)
+    ses_bids = re.search(r'ses-\d+', saxe_fname).group(0)
+    run_bids = re.search(r'run-\d+', saxe_fname).group(0)
+    task_name = re.search(r'run-\d+-(\w+)_beh', saxe_fname).group(1)
+        # beh_fpath = join(beh_inputdir, sub, f"{sub}_{ses}_task-fractional_{run_bids}-tomsaxe_beh.csv")
+    beh_df = pd.read_csv(saxe_fpath)
+    subset_beh = beh_df[['event02_filetype', 'event02_story_onset','event03_question_onset', 'event04_response_onset','event04_RT','accuracy']]
 
-# belief, photo, rating, accuracy as covariate
-subset_belief = pd.DataFrame(); subset_photo = pd.DataFrame(); subset_rating = pd.DataFrame(); 
+    # belief, photo, rating, accuracy as covariate
+    subset_belief = pd.DataFrame(); subset_photo = pd.DataFrame(); subset_rating = pd.DataFrame(); 
 
-subset_belief['onset'] = subset_beh.loc[subset_beh.event02_filetype == 'false_belief', 'event02_story_onset']
-subset_belief['duration'] = 11
-subset_belief['trial_type'] = 'false_belief'
-subset_belief['pmod_accuracy'] = subset_beh.loc[subset_beh.event02_filetype == 'false_belief', 'accuracy']
+    subset_belief['onset'] = subset_beh.loc[subset_beh.event02_filetype == 'false_belief', 'event02_story_onset']
+    subset_belief['duration'] = 11
+    subset_belief['trial_type'] = 'false_belief'
+    subset_belief['pmod_accuracy'] = subset_beh.loc[subset_beh.event02_filetype == 'false_belief', 'accuracy']
 
-subset_photo['onset'] = subset_beh.loc[subset_beh.event02_filetype == 'false_photo', 'event02_story_onset']
-subset_photo['duration'] = 11
-subset_photo['trial_type'] = 'false_photo'
-subset_photo['pmod_accuracy'] = subset_beh.loc[subset_beh.event02_filetype == 'false_photo', 'accuracy']
+    subset_photo['onset'] = subset_beh.loc[subset_beh.event02_filetype == 'false_photo', 'event02_story_onset']
+    subset_photo['duration'] = 11
+    subset_photo['trial_type'] = 'false_photo'
+    subset_photo['pmod_accuracy'] = subset_beh.loc[subset_beh.event02_filetype == 'false_photo', 'accuracy']
 
-subset_rating['onset'] = subset_beh.event03_question_onset
-subset_rating['duration'] = subset_beh.event04_RT
-subset_rating['trial_type'] = 'rating' #'rating_' + subset_beh['event02_filetype'].str.replace('_', '')
-subset_rating['pmod_accuracy'] = subset_beh.accuracy #'rating_' + subset_beh['event02_filetype'].str.replace('_', '')
+    subset_rating['onset'] = subset_beh.event03_question_onset
+    subset_rating['duration'] = subset_beh.event04_RT
+    subset_rating['trial_type'] = 'rating' #'rating_' + subset_beh['event02_filetype'].str.replace('_', '')
+    subset_rating['pmod_accuracy'] = subset_beh.accuracy #'rating_' + subset_beh['event02_filetype'].str.replace('_', '')
 
-# concatenate above dataframes and save in new folder
-events = pd.concat([subset_belief, subset_photo, subset_rating], ignore_index=True)
+    # concatenate above dataframes and save in new folder
+    events = pd.concat([subset_belief, subset_photo, subset_rating], ignore_index=True)
+    
+    beh_savedir = join(main_dir, 'data' , 'beh', 'beh03_bids', sub_bids)
+    Path(beh_savedir).mkdir( parents=True, exist_ok=True )
+    # extract bids info and save as new file
+    events.to_csv(join(beh_savedir, f"{sub_bids}_{ses_bids}_task-{task_name}_{run_bids}_events.tsv"), sep='\t', index=False)
 
-# extract bids info and save as new file
-string = os.path.basename(beh_fpath)
-sub_bids = re.search(r'sub-\d+', string).group(0)
-ses_bids = re.search(r'ses-\d+', string).group(0)
-run_bids = re.search(r'run-\d+', string).group(0)
-task_name = re.search(r'run-\d+-(\w+)_beh', string).group(1)
-events.to_csv(join(beh_savedir, f"{sub_bids}_{ses_bids}_task-{task_name}_{run_bids}_events.tsv"), sep='\t', index=False)
+
 
 # %%
