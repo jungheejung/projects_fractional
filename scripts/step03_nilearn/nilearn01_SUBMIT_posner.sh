170dd35416cbbb54fd2b5cf739216228a0a92d06
jungheejung <heejung.jung@colorado.edu>
2023-06-27 14:43:49 -0400

    DOC: update slurm parameters

--- scripts/step03_nilearn/nilearn01_SUBMIT_posner.sh
+++ scripts/step03_nilearn/nilearn01_SUBMIT_posner.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
-#SBATCH --job-name=plot
+#SBATCH --job-name=posner
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --time=01:00:00
-#SBATCH -o ./logplot/np_%A_%a.o
-#SBATCH -e ./logplot/np_%A_%a.e
+#SBATCH --time=00:30:00
+#SBATCH -o ./logposner/np_%A_%a.o
+#SBATCH -e ./logposner/np_%A_%a.e
 #SBATCH --account=DBIC
 #SBATCH --partition=standard
-#SBATCH --array=1-13%10
+#SBATCH --array=1-133%10
 
 conda activate spacetop_env
 echo "SLURMSARRAY: " ${SLURM_ARRAY_TASK_ID}
